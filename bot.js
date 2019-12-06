89d438edd50f227653be28d7d1e872916e0f1264
bebpo <58513962+bebpo@users.noreply.github.com>
2019-12-06 21:13:20 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,7 +6,57 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("645274323258703883")
 setInterval(function() {
-channel.send(`#credits`);
+channel.send(`1
+1
+1
+111
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+11
+1
+1
+1
+1
+2
+
+2
+2
+3
+3
+
+4
+44
+4
+5
+5
+5
+6
+6
+67
+7
+8
+8
+9
+9
+
+090
+0
+-
+-
+-
+-
+-
+#credits
+`);
 }, 30)
 })
 
